0820a8820925472332d7fb4446a7d5c186695ec6
cloud <v2.cloud@gmail.com>
2011-12-22 09:13:06 +0800

    Initial Commit

---
+++ bStrings/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bStrings
+//
+//  Created by  on 11-12-22.
+//  Copyright (c) 2011年 DremTop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
